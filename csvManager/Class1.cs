a603f2172e328de95888122e515998b8a0084bf5
kaw393939 <keith@webizly.com>
2020-03-06 10:48:56 -0500

    CSV Reader

---
+++ csvManager/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using CsvHelper;
+
+namespace csvManager
+{
+    public class Read
+    {
+
+        public static IList<T> getRecords<T>(string _filename)
+        {
+            IList<T> _records = new List<T>();
+
+            using (var reader = new StreamReader(_filename))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                while (csv.Read())
+
+                {
+                    _records.Add(csv.GetRecord<T>());
+
+                }
+            }
+
+            return _records;
+        }
+
+    }
+}
---
+++ csvManagerTests/ReadTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace csvManager.Tests
+{
+    [TestClass()]
+    public class ReadTests
+    {
+        [TestMethod()]
+        public void getRecordsTest()
+        {
+            var _filename = "/Users//keith//OneDrive//Documents//csvData//test.csv";
+            var records = Read.getRecords<Foo>(_filename);
+            Debug.Write(records.ToString());
+        }
+
+        public class Foo
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+
+        }
+    }
+}
